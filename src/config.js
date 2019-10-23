ae3727dad03af57da6ab40c6897738c760d9c051
dorage <baloonflower554@gmail.com>
2019-10-24 04:07:20 +0900

    redirect url

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ export const googleConfig = {
     clientID:
         '759671542548-3oridq879efogvduoid8jiqkh2l3o4bo.apps.googleusercontent.com',
     clientSecret: 'i3X6S_8DzLK8m6NaFG3P4dji',
-    callbackURL: `${SERVER_URL}/google/callback`,
+    callbackURL: '/google/callback',
 };
 export const facebookConfig = {
     clientID: '2499202380362030',
     clientSecret: 'bc10db4ad2191f09a3107489b00a75e4',
-    callbackURL: `${SERVER_URL}/facebook/callback`,
+    callbackURL: '/facebook/callback',
 };
