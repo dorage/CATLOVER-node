0aca3492257d9b54b9dfddf231dcd544cbd38d8c
dorage <baloonflower554@gmail.com>
2019-10-29 20:23:33 +0900

    google auth callbackURL

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const googleConfig = {
     clientID:
         '759671542548-3oridq879efogvduoid8jiqkh2l3o4bo.apps.googleusercontent.com',
     clientSecret: 'i3X6S_8DzLK8m6NaFG3P4dji',
-    callbackURL: '/google/callback',
+    callbackURL: 'https://api.catloverlife.com/google/callback',
 };
 export const facebookConfig = {
     clientID: '2499202380362030',
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Server from './expressApp';
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8383;
 
 const handleListen = () => {
     console.log(`Listening On : http://localhost:${port}`);
